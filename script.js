0864333c54216e1616f58e5fa743856fe7e95999
Tomas Gonzalez <tomasgonz875@gmail.com>
2020-02-04 16:53:54 -0500

    Able to get all questions and their corresponding correct answer.

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 var questionsHTML = document.getElementsByClassName('question_text user_content enhanced');
 var answersHTML = document.getElementsByClassName('correct_answer');
 
-// loop to get all the question texts
-for (var i = 0; i < questionsHTML.length; i++) {
-    console.log(questionsHTML[i].innerHTML);
-  }
-
-// loop to get all the correct answer texts, even if I selected the wrong answer on the quiz
-for (var i = 0; i < answersHTML.length; i++) {
-    var a = answersHTML[i].getElementsByClassName('answer_text');
-    console.log(a[0].innerHTML);
+if (answersHTML.length == questionsHTML.length) {
+    for (var i = 0; i < answersHTML.length; i++) {
+        var a = answersHTML[i].getElementsByClassName('answer_text');
+        console.log(questionsHTML[i].innerHTML + "\nAnswer: " + a[0].innerHTML);
+    }
+}
+else {
+    console.log('ERROR: Questions and Answers are not the same size.');
 }
